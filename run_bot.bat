78f5865b0d5a8cb17fa014bd1b0f2de4ee41364f
mohammad <mohammad@hero.best>
2025-07-10 14:04:55 +0330

    test

---
+++ run_bot.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d E:\telegram-user-bot
+"C:\Users\Hepta\AppData\Local\Programs\Python\Python310\python.exe" time_bot.py
+pause
---
+++ time_bot.py
@@ -0,0 +1,92 @@
+from PIL import Image, ImageDraw, ImageFont
+from telethon import TelegramClient, functions
+import asyncio
+import datetime
+import pytz
+
+api_id = 26865532
+api_hash = '4db5fd680068290104076d1e80511638'
+
+client = TelegramClient('mohammad_session', api_id, api_hash)
+
+def to_digital_font(text):
+    digits_map = {
+        '０': '𝟶',
+        '１': '𝟷',
+        '２': '𝟸',
+        '３': '𝟹',
+        '４': '𝟺',
+        '５': '𝟻',
+        '６': '𝟼',
+        '７': '𝟽',
+        '８': '𝟾',
+        '９': '𝟿',
+        '：': ':'
+    }
+    return ''.join(digits_map.get(c, c) for c in text)
+
+def create_profile_pic_with_time(base_image_path, output_path, time_text):
+    img = Image.open(base_image_path).convert('RGBA')
+    draw = ImageDraw.Draw(img)
+
+    font = ImageFont.truetype('digital-7.ttf', 50)  
+
+    width, height = img.size
+    text_width, text_height = draw.textsize(time_text, font=font)
+    x = (width - text_width) / 2
+    y = height - text_height - 20  
+
+    draw.text((x, y), time_text, font=font, fill=(255, 255, 255, 255))
+
+    img.save(output_path)
+
+def get_iran_time():
+    tehran = pytz.timezone("Asia/Tehran")
+    return datetime.datetime.now(tehran)
+
+async def sleep_until_next_minute():
+    now = get_iran_time()
+    seconds = 60 - now.second
+    await asyncio.sleep(seconds)
+
+async def update_forever():
+    await client.start()
+    
+    base_image = 'base.jpg'  
+    output_image = 'profile_with_time.png'  
+
+    while True:
+        now = get_iran_time()
+        time_str = now.strftime('%H:%M')
+        digital_time = to_digital_font(time_str)
+        
+        create_profile_pic_with_time(base_image, output_image, digital_time)
+        try:
+            file = await client.upload_file(output_image)
+            await client(functions.photos.UploadProfilePhotoRequest(file))
+            print(f'✅ عکس پروفایل آپدیت شد: {time_str}')
+        except Exception as e:
+            print(f'❌ خطا در آپدیت عکس پروفایل: {e}')
+            
+        fancy_name = f'MOHAMMAD | {digital_time}'
+        fancy_bio = f'دیگه پسر خوبی شدم :) | {digital_time}'
+
+        try:
+            await client(functions.account.UpdateProfileRequest(
+                first_name=fancy_name,
+                about=fancy_bio
+            ))
+            print(f"✅ آپدیت شد → {fancy_name}")
+        except Exception as e:
+            print(f"❌ خطا: {e}")
+
+        await sleep_until_next_minute()
+    
+
+async def main():
+    await client.start()
+    await update_forever()
+
+
+with client:
+    client.loop.run_until_complete(main())
